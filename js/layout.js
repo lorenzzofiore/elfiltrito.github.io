1be23590e217f9b9db7755737c200621fdfb081b
Lorenzo <104233199+lorenzzofiore@users.noreply.github.com>
2023-01-20 19:26:50 -0300

    Add files via upload

--- js/layout.js
+++ js/layout.js
@@ -94,3 +94,98 @@ class footer extends HTMLElement {
 }
 
 window.customElements.define('template-footer', footer);
+
+
+/*############# CARDS CONTAINER ###############*/
+
+const templateCards = document.createElement('Template');
+templateCards.innerHTML = `
+
+<div class="contenedorcards">
+
+<section class="categorias wow animate__animated animate__bounceInLeft">
+    <article class="card-filtros">
+        <h3 class="title">Filtros</h3>
+        <a href="filtros.html" class="imgfiltros"><img src="img/filtros.svg" width="250px" alt="filtros"></a>
+    </article>
+    
+    <article class="card-stickers">
+        <h3 class="title">Stickers</h3>
+        <a href="stickers.html" class="imgstickers"><img src="img/sticker.svg" height="280px" alt="stickers"></a>
+    </article>
+
+    <article class="card-personalizados">
+        <div class="contentimg">   
+            <a href="personaliza.html" class="imgpersonalizados"><img src="img/personalizados.png" alt="filtros"></a>
+            </div>
+        <div class="contentext">
+            <h3 class="title">Filtros Personalizados</h3>
+            <p class="text">Envianos la imagen y nosotros nos encargamos de realizarlos!</p> <br>
+            <a href="mailto:filtritoba@gmail.com" class="btn-personalizar"><button>Personalizar mis Filtros</button></a>
+        </div> 
+    </article>
+</section>
+
+<section class="especiales wow animate__animated animate__bounceInRight">
+
+    <article class="card-t-skvio">
+        <div class="contentimg">   
+            <a href="t-skvio.html" class="t-skavio"><img src="img/t-skvio.svg" width="260px" alt="filtros"></a>
+        </div>
+        <div class="contentext">
+            <h3 class="title">T-SKVIO</h3>
+            <p class="text">Un juego para escabiar y cagarte de risa con tus amigos!</p><br>
+            <a href="t-skavio.html" class="btn-personalizar"><button>Visitar T-SKVIO</button></a>
+        </div>
+    </article>
+
+    <article class="card-filtros3d">
+        <a href="stickers.html" class="imgfiltro3d"><img src="img/elsensei.svg"  height="250px" alt="3D"></a>
+        <button class="elsensei" data-modal-target="#modal">Comprar Semillas</button>
+    </article>
+
+<!-- POP UP CON INFORMACION DE CONTACTO A ELSENSEI SEMILLAS-->
+
+    <div class="modal" id="modal">
+        <div class="modal-header">
+            <div class="title">¿Cultivas? <br> El Sensei tiene lo que necesitas</div>
+            <button data-close-button class="close-button">&times;</button>
+        </div>
+        <div class="modal-body">
+            <article class="card-instagram">
+                <h3 class="title">Ir a Instagram de <br> El Sensei</h3> 
+                <a href="https://www.instagram.com/elsenseiseeds/?hl=es-la" class="imgstickers"><img src="img/instasensei.svg" height="90px" alt="stickers"></a>
+            </article>
+
+            <article class="card-mail">
+                <a href="" class="imgstickers"><img src="img/mailsensei.svg" height="90px" alt="stickers"></a>
+                <h3 class="title">Pedir Catalogo al Mail</h3>
+                <br>
+                <a href="mailto:elsenseiseeds@gmail.com"><button type="submit" class="signup-input-submit">Escribir Email</button></a>
+            </article>
+
+        </div>
+    </div>
+    <div id="overlay"></div>
+
+<!-- TERMINA POP UP CON INFO CONTACT ELSENSEI SEMILLAS-->
+
+    <article class="card-specialsticker">
+        <a href="stickers.html" class="imgstickers"><img src="img/specialsticker.svg" width="220px" alt="stickers"></a>
+        <h3 class="title">Special Stickers</h3>
+        
+    </article>
+
+</section>
+</div>`;
+
+class contenedorcards extends HTMLElement {
+    constructor() {
+        super();
+
+        const cards = templateCards.content.cloneNode(true);
+        this.appendChild(cards);
+    }
+}
+
+window.customElements.define('template-cards', contenedorcards);
